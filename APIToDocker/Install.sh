e7d317559ead9d948217368305dab3478ea84452
punjabisingh <harinder@me.com>
2018-06-27 16:47:34 +0800

    changed port from 80 to 3000

--- APIToDocker/Install.sh
+++ APIToDocker/Install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+
 docker stop API
 docker rmi apitodocker --force
 docker rm API
 docker build -t apitodocker .
-docker run -d -p 8080:80 --name API apitodocker
+docker run -d -p 8080:3000 --name API apitodocker
 docker network connect simple-network API
