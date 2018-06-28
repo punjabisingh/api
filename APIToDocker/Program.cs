f90a906d092cfeff067b31addc28640fcfa3eae2
punjabisingh <harinder@me.com>
2018-06-28 10:16:03 +0800

    updated

--- APIToDocker/Program.cs
+++ APIToDocker/Program.cs
@@ -14,11 +14,29 @@ namespace APIToDocker
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var config = new ConfigurationBuilder().AddEnvironmentVariables("").Build();
+            // 2nd line added
+
+            var url = config["ASPNETCORE_URLS"] ?? "http://*:3000";
+            // 3rd line added
+
+            var host = new WebHostBuilder()
+            .UseKestrel()
+            .UseContentRoot(Directory.GetCurrentDirectory())
+            .UseIISIntegration()
+                .UseStartup<Startup>()
+            .UseUrls(url) // 4th line added
+            .Build();
+
+            host.Run();
+            //BuildWebHost(args).Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                   .UseKestrel()
+                   .UseContentRoot(Directory.GetCurrentDirectory())
+                   .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
     }
